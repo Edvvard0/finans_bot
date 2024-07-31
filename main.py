4b7764ffa1ed00e0e9a422b33874fb2352f2ae36
Edvvard0 <edward.osipchukzzz@gmail.com>
2024-07-31 13:03:17 +0300

    создал функцию funk_markup и вынес в неё создание кнопок

--- main.py
+++ main.py
@@ -17,6 +17,16 @@ sport = []
 other = []
 
 
+def func_markup():
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("/spend")
+    item2 = types.KeyboardButton("/cash")
+    markup.row(item1, item2)
+    item3 = types.KeyboardButton("/balance")
+    markup.add(item3)
+    return markup
+
+
 def verify_many(message, x):
     if not x.isdigit() or x[0]==0:
           bot.send_message(message.chat.id, 'некоректный ввод данных')
@@ -26,23 +36,11 @@ def verify_many(message, x):
 
 @bot.message_handler(commands=['start'])
 def spend(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton("/spend")
-    item2 = types.KeyboardButton("/cash")
-    markup.row(item1,item2)
-    item3 = types.KeyboardButton("/balance")
-    markup.add(item3)
+    markup = func_markup()
     bot.send_message(message.chat.id,'привет я бот для ведения учета твойх финансов',reply_markup=markup)
 
 @bot.message_handler(commands=['balance'])
 def balance(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton("/spend")
-    item2 = types.KeyboardButton("/cash")
-    markup.row(item1, item2)
-    item3 = types.KeyboardButton("/balance")
-    markup.add(item3)
-
     bot.reply_to(message, f"Ваш баланс: {sum(finans)-sum(expenses)},\n"
                           f"вы потратили {sum(expenses)} \n"
                           f"вы заработали {sum(finans)}")
@@ -59,21 +57,12 @@ def get_spend(message):
         if verify_many(message, amount):
             expenses.append(int(amount))
 
-            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            item1 = types.KeyboardButton("/spend")
-            item2 = types.KeyboardButton("/cash")
-            markup.row(item1, item2)
-            item3 = types.KeyboardButton("/balance")
-            markup.add(item3)
-
+            markup = func_markup()
             bot.reply_to(message, f"расход на сумму {amount} добавлен",reply_markup=markup)
-
 
 
 @bot.message_handler(commands=['cash'])
 def finanses(message):
-
-
     bot.send_message(message.chat.id, 'сколько ты заработал')
     bot.register_next_step_handler(message, get_finans)
 
@@ -82,16 +71,8 @@ def get_finans(message):
         if verify_many(message, amount):
             finans.append(int(amount))
 
-            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            item1 = types.KeyboardButton("/spend")
-            item2 = types.KeyboardButton("/cash")
-            markup.row(item1, item2)
-            item3 = types.KeyboardButton("/balance")
-            markup.add(item3)
-
+            markup = func_markup()
             bot.reply_to(message, f"Доход на сумму {amount} добавлен",reply_markup=markup)
-
-
 
 
 # Запуск телеграм бота
